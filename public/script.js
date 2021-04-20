74c52fcc92b1b9c5d3ecd7932e8a07b3f1f733bc
Harshit <gupta.harshit99@gmail.com>
2021-04-20 22:02:09 +0530

    upd

--- public/script.js
+++ public/script.js
@@ -1,24 +1,28 @@
 // const axios = require("axios");
 
-let nextBtn =  document.querySelector("a");
-let input = document.querySelector("input");
-// let db = require("./info")
+// let nextBtn =  document.querySelector("a");
+// let input = document.querySelector("input");
+// // let db = require("./info")
 
-console.log(db);
+// console.log(db);
 
-nextBtn.addEventListener("click",function(){
+// nextBtn.addEventListener("click",function(){
     
-    console.log(db);
-    db.name = input.value;
-    nextQues(db);
+//     console.log(db);
+//     db.name = input.value;
+//     nextQues(db);
 
-});
+// });
 
 
-function nextQues(db){
-    let url = window.location.href;
-    url =  url.split("/");
-    url = url[3];
-    console.log(url);
-    axios.get("./Q2.html",db);
-}
+// function nextQues(db){
+//     let url = window.location.href;
+//     url =  url.split("/");
+//     url = url[3];
+//     console.log(url);
+//     axios.get("./Q2.html",db);
+// }
+
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
